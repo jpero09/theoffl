c9ab09c29484e72bad928b978f298dabd60a84e7
Jason Pero <jpero09@gmail.com>
2016-12-21 16:18:24 +0000

    added logentries support

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var http = require('http');
+var Logger = require('le_node');
 
 var app = express();
 var pkgjson = require('./package.json');
+
+var log = new Logger({
+  token:'1b531217-af39-4577-a1e1-2b10081beda7',
+  console: true,
+  timestamp: true
+});
 
 app.set('name', pkgjson.name);
 app.set('version', pkgjson.version);
 app.set('host', process.env.HOST || process.env.IP || 'localhost');
 app.set('port', process.env.PORT || 3000);
+app.set('env', process.env.NODE_ENV || 'dev');
 
 app.get('/', function (req, res) {
   res.send('Future home of the "Ohio Fantasy Football League"!')
@@ -15,8 +23,8 @@ app.get('/', function (req, res) {
 
 // Start this party:
 http.createServer(app).listen(app.get('port'), app.get('host'), function() {
-  console.log('Starting: %s v%s', app.get('name'), app.get('version'));
-  console.log('Running @ //%s:%s', app.get('host'), app.get('port'));
-  console.log('Environment:', process.env.NODE_ENV);
-  console.log('Software:', process.versions);
+  log.info(`Starting: ${app.get('name')} v${app.get('version')}`);
+  log.info(`Running @ //${app.get('host')}:${app.get('port')}`);
+  log.info(`Environment: ${app.get('env')}`);
+  log.debug(`Software: ${JSON.stringify(process.versions)}`);
 });
