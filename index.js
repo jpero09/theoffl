56d23449ff780fab2cc7ff3cb2876f6d03e1dd69
Jason Pero <jpero09@gmail.com>
2016-12-21 17:40:59 +0000

    alt express start emthod

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var Logger = require('le_node');
 
 var app = express();
 var pkgjson = require('./package.json');
+var env = process.env.NODE_ENV || 'dev';
 
 var log = new Logger({
   token: process.env.LOG_ENTRIES_TOKEN,
-  console: true,
+  console: (env === 'dev'),
   timestamp: true
 });
 
@@ -15,14 +16,14 @@ app.set('name', pkgjson.name);
 app.set('version', pkgjson.version);
 app.set('host', process.env.HOST || process.env.IP || 'localhost');
 app.set('port', process.env.PORT || 3000);
-app.set('env', process.env.NODE_ENV || 'dev');
+app.set('env', env);
 
 app.get('/', function (req, res) {
   res.send('Future home of the "Ohio Fantasy Football League"!')
 });
 
 // Start this party:
-http.createServer(app).listen(app.get('port'), app.get('host'), function() {
+app.listen(app.get('port'), function() {
   log.info(`Starting: ${app.get('name')} v${app.get('version')}`);
   log.info(`Running @ //${app.get('host')}:${app.get('port')}`);
   log.info(`Environment: ${app.get('env')}`);
